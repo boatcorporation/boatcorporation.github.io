3798e3c370a227b97cc3d55f0db51125b46385bf
AD1641 <ad1641@york.ac.uk>
2022-01-20 17:47:29 +0000

    Setup of Libgdx & Screens

---
+++ desktop/src/com/boatgame/io/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.boatgame.io.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.boatgame.io.boatGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new boatGame(), config);
+	}
+}
